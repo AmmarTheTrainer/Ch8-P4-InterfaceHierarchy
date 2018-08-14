08933f4a0a045fef0c74038dcb9219d26e03f086
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-08-14 11:31:22 +0500

    first commit from hostel

---
+++ Ch8-P4-InterfaceHierarchy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8_P4_InterfaceHierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
